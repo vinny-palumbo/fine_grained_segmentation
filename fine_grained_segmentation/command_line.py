528689ccb558c9eb07543dda7b5ede4d4ca78f21
Vincent Palumbo <vinnypalumbo.com@gmail.com>
2019-10-29 18:31:54 -0400

    Pass image path as an argument to CLI to run on any image

--- fine_grained_segmentation/command_line.py
+++ fine_grained_segmentation/command_line.py
@@ -3,7 +3,7 @@ from fine_grained_segmentation.main import detect
 
 
 def main():
-    '''
+    
     # Parse command line arguments
     parser = argparse.ArgumentParser(
         description='Detect and segment fashion items in images.')
@@ -13,8 +13,6 @@ def main():
 
     args = parser.parse_args()
     detect(args.image)
-    '''
-    detect("https://cdn.cliqueinc.com/cache/posts/281219/cool-outfits-for-women-in-20s-281219-1562964559214-main.600x0c.jpg")
 
 
 if __name__ == "__main__":
